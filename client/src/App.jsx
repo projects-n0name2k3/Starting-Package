84dced265cae822213c6f6799348204af27d848a
Noname2k4 <nva.tiktok90@gmail.com>
2024-02-23 21:41:00 +0700

    add react-router-dom + redux + redux persist + basic route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,23 @@
-function App() {
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import PrivateRoute from "./components/PrivateRoute";
+const App = () => {
   return (
-    <>
-      <p className="text-red-500 text-3xl">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<SignIn />} />
+        <Route path="/register" element={<SignUp />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
